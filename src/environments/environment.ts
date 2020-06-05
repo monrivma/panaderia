eb056d9a18f12f44bcff892ee6c2e5c85d6bc4eb
monrivma <48312856+monrivma@users.noreply.github.com>
2020-06-05 12:44:10 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebaseConfig : {
+    apiKey: 'AIzaSyBgaJXPsFZ5wQw-6zpZhMZujJDgOjSAiLI',
+    authDomain: 'panaderia-caf90.firebaseapp.com',
+    databaseURL: 'https://panaderia-caf90.firebaseio.com',
+    projectId: 'panaderia-caf90',
+    storageBucket: 'panaderia-caf90.appspot.com',
+    messagingSenderId: '77387597642',
+    appId: '1:77387597642:web:8dc26f0e3608f9785063b3',
+    measurementId: 'G-FWWRXCXX3X'
+  }
 };
 
 /*
